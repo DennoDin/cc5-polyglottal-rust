848eb6ed3f8f2f0b79000cadcf6b27aae7020d77
Dustin <dustin.tran@live.com>
2018-08-30 11:48:35 +0900

    NOT WORKING:created multiple resources and route paths

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+// extern crate actix;
 extern crate actix_web;
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
 
-use actix_web::{server, App, HttpRequest};
+use actix_web::*;
+// use bytes::Bytes;
+// use futures::stream::once;
+// use futures::future::{Future, result};
 
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello, world!"
+use actix_web::{server, App, HttpRequest, HttpResponse, Error, Responder, http::*};
+
+#[derive(Serialize)]
+struct MyObj {
+    name: &'static str
+}
+
+impl Responder for MyObj {
+    type Item = HttpResponse;
+    type Error = Error;
+
+    fn respond_to<S>(self, req: &HttpRequest<S>) -> Result<HttpResponse, Error> {
+        let body = serde_json::to_string(&self)?;
+
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body))
+    }
+}
+
+fn index(_req: &HttpRequest) -> impl Responder {
+    MyObj{
+        name: "Dustin"
+    }
 }
 
 fn home(_req: &HttpRequest) -> &'static str {
@@ -17,7 +46,13 @@ fn main() {
                 .prefix("/api")
                 .resource("/", |r| r.f(index)),
             App::new()
-                .resource("/", |r| r.f(home))
+                .resource("/", |r| {
+                    r
+                        .route("", Method::GET, home)
+                        .route("/index", Method::GET, index)
+                })
+                //r.f(home))
+                // .resource("/index", |r| r.f(index))
         ]
     });
     
